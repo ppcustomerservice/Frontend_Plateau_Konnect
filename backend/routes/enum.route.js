e82c5b8a12499aa8ec82040e4fa88ef281198e47
ppcustomerservice <customerservice@propertyplateau.com>
2025-05-13 16:40:15 +0530

    Delete backend directory

--- backend/routes/enum.route.js
+++
@@ -1,8 +0,0 @@
-const express =require("express");
-const { getAllEnums } =require("../controllers/enum.controller.js");
-
-const router=express.Router();
-
-router.get("/",getAllEnums);
-
-module.exports = router;
